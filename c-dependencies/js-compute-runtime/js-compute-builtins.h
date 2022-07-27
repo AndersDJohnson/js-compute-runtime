70c6e2ca39f1cbfd7bdc8284317f85bfe7731431
Jake Champion <jchampion@fastly.com>
2022-07-27 13:30:36 +0100

    allow use of value_to_buffer from other files by adding it to the header file
    we plan to move the built-ins into their own files and they will require access to this functions

--- c-dependencies/js-compute-runtime/js-compute-builtins.h
+++ c-dependencies/js-compute-runtime/js-compute-builtins.h
@@ -53,6 +53,9 @@ inline bool ThrowIfNotConstructing(JSContext *cx, const JS::CallArgs &args, cons
   JS_ReportErrorNumberASCII(cx, GetErrorMessage, nullptr, JSMSG_BUILTIN_CTOR_NO_NEW, builtinName);
   return false;
 }
+
+uint8_t *value_to_buffer(JSContext *cx, JS::HandleValue val, const char *val_desc, size_t *len);
+
 bool hasWizeningFinished();
 bool isWizening();
 void markWizeningAsFinished();
