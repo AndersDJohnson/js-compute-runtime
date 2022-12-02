e428f4f82cef463a83bb91d41a48e0e0ba377a9c
Trevor Elliott <telliott@fastly.com>
2022-12-02 14:42:33 -0800

    Updates for newer spidermonkey

--- c-dependencies/js-compute-runtime/js-compute-builtins.cpp
+++ c-dependencies/js-compute-runtime/js-compute-builtins.cpp
@@ -3412,7 +3412,7 @@ bool init_class(JSContext *cx, HandleObject global) {
     return false;
 
   JS::SymbolCode code = JS::SymbolCode::iterator;
-  JS::RootedId iteratorId(cx, SYMBOL_TO_JSID(JS::GetWellKnownSymbol(cx, code)));
+  JS::RootedId iteratorId(cx, JS::GetWellKnownSymbolKey(cx, code));
   return JS_DefinePropertyById(cx, proto_obj, iteratorId, entries, 0);
 }
 
